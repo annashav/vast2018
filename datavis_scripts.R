971e780a37bb77330702940167820fea9f72f097
andrewtran1 <39430866+andrewtran1@users.noreply.github.com>
2018-06-04 19:41:54 -0700

    More visualizations

--- datavis_scripts.R
+++ datavis_scripts.R
@@ -8,15 +8,17 @@ library(plot3D)
 
 #CHANGE TO YOUR DIRECTORY
 BASE            <- "C:\\Users\\Andrew\\Documents\\Github\\vast2018\\"
-CSV_PATH        <- paste(BASE, "metadata\\AllBirds_Hour.csv", sep="")
+CSV_PATH        <- paste(BASE, "metadata\\AllBirds_Vocalization.csv", sep="")
 TEST_BIRDS_PATH <- paste(BASE, "Test Birds Location.csv",     sep="")
 IMAGE_PATH      <- paste(BASE, "Lekagul Roadways 2018.png",   sep="")
 
 X_MAX <- 200
 Y_MAX <- 200
 DUMP  <- c(x=148, y=159) #Location of alleged dump site.
-ALL_QUALITIES <- c("A", "B", "C", "D", "E")
-ALL_SPECIES   <- c(
+ALL_QUALITIES  <- c("A", "B", "C", "D", "E", "no score")
+QUALITY_COLORS <- c("Purple", "Blue", "Green", "Yellow", "Orange", "Black")
+
+ALL_SPECIES    <- c(
  "Bent-beak Riffraff", "Blue-collared Zipper",    "Bombadil",
  "Broad-winged Jojo",  "Canadian Cootamum",       "Carries Champagne Pipit",
  "Darkwing Sparrow",   "Eastern Corn Skeet",      "Green-tipped Scarlet Pipit",
@@ -36,7 +38,16 @@ SPECIES_COLORS <- c(
 )
 names(SPECIES_COLORS) <- ALL_SPECIES
 
-dtable             <- read.csv(CSV_PATH, header=TRUE)
+ALL_VOCALIZATIONS <- c(
+  "\"call, song\"", "bill-snapping", "call", "drumming", "scold", "song"
+)
+VOCALIZATION_COLORS <- c(
+  "purple", "orange", "blue", "green", "black", "red"
+)
+names(VOCALIZATION_COLORS) <- ALL_VOCALIZATIONS
+
+dtable <- read.csv(CSV_PATH, header=TRUE)
+
 #Plot the locations of the bird call recordings.
 #
 #title         - title for plot
@@ -107,34 +118,40 @@ generate_histograms <- function(dtable, column, collection, filter, breaks, ylim
      title    <- paste("Histogram of", column, "for", element)
      out_file <- paste(out_dir, "Hist_", column, "_", element, ".png", sep="")
 
-     #freq_table <- table(filtered[["Year"]])
-     #write.csv(freq_table, out_file, row.names = FALSE)
-     
      png(out_file)
      hist(filtered[[column]], main=title, xlab=column, ylim=c(0,60), col="lightblue", breaks=breaks)
      dev.off()
   }
 }
 
+plot_xy(dtable, "All Species Colored by Recording Quality", QUALITY_COLORS[dtable[["Quality"]]])
+#plot_xy(dtable, "All Species Colored by Call Type", VOCALIZATION_COLORS[dtable[["Vocalization_type"]]])
+#Generate histogram of all data
+#hist(dtable[["Year"]], main="Histogram of Year of Recording", xlab="Year", col="lightblue", breaks=1983:2018)
+#hist(dtable[dtable[["Month"]] != 0,][["Month"]], main="Histogram of Month of Recording", xlab="Month", col="lightblue", breaks=1:12)
+#hist(dtable[["Hour"]], main="Histogram of Hour of Recording", xlab="Hour", col="lightblue", breaks=0:23)
+
+#Histogram of year, number of recordings from before April
+#beforeMarch <- dtable[dtable[["Month"]] <= 3, ]
+#hist(beforeMarch[["Year"]], main="Histogram of Years from Jan to Mar", xlab="Year", ylim=c(0,60), col="lightblue", breaks=1983:2018)
+#table[beforeMarch[["Year"]])
+
 filter1 <- function(dtable, specie)
 {
   dtable <- dtable[dtable[["English_name"]] == specie,]
   dtable[dtable[["Year"]] != 2018,]
 }
-
-
 filter2 <- function(dtable, specie)
 {
   dtable <- dtable[dtable[["English_name"]] == specie,]
 }
-
-
+#Generate histogram of all species based on filter.
 #generate_histograms(dtable, "Year", ALL_SPECIES, filter1, 1983:2017, c(0,60), paste(BASE, "histograms\\", sep=""))
-generate_histograms(dtable, "Hour", ALL_SPECIES, filter2, 0:24, c(0,100), paste(BASE, "histograms3\\", sep=""))
-#generate_maps(paste(BASE, "Hist_Months\\", sep=""))
+#generate_histograms(dtable, "Hour", ALL_SPECIES, filter2, 0:24, c(0,100), paste(BASE, "histograms3\\", sep=""))
 
-#vocal_freq <- table(dtable[["Vocalization_type"]])[c(3, 6, 1, 4, 2, 5)]
-#barplot(vocal_freq, col="lightblue", main="Histogram of Vocalization Typ)e", ylab="Frequency", xlab="Vocalization Type")
+#Generate histogramm of the vocalization types
+vocal_freq <- table(dtable[["Vocalization_type"]])[c(3, 6, 1, 4, 2, 5)]
+barplot(vocal_freq, col=VOCALIZATION_COLORS[vocal_freq], main="Histogram of Vocalization Typ)e", ylab="Frequency", xlab="Vocalization Type")
 
 #hour      <- as.numeric(dtable[["Hour"]])
 #frequency <- table(dtable[["Year"]], dtable[["Time"]])
